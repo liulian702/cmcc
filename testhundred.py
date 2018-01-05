b35f67b38ada00f0742f340fe000d55253de5328
liulian701 <liulian701@163.com>
2018-01-05 11:21:22 +0800

    add *.py

---
+++ testhundred.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+number = int(input("Enter an number:"))
+if number <= 100:
+    print("your number is smaller than equal to 100")
+else:
+    print("Your number is greater than 100")
